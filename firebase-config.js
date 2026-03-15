0928b33eeaac2b0f8f7fb4d6313fa2e04d9658e4
triedourluck <notfortheradios@gmail.com>
2026-03-15 19:08:03 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -3,12 +3,12 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.1/firebase
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-storage.js";
 
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_AUTH_DOMAIN",
-  projectId: "TU_PROJECT_ID",
-  storageBucket: "TU_STORAGE_BUCKET",
-  messagingSenderId: "TU_MESSAGING_SENDER_ID",
-  appId: "TU_APP_ID"
+  apiKey: "AIzaSyDxT8Yoq7wwEFZ3236DzoIvAw0twfboTtc",
+  authDomain: "inthedarkigrew-29bf8.firebaseapp.com",
+  projectId: "inthedarkigrew-29bf8",
+  storageBucket: "inthedarkigrew-29bf8.firebasestorage.app",
+  messagingSenderId: "761080369460",
+  appId: "1:761080369460:web:567825de5b83bea5465561"
 };
 
 const app = initializeApp(firebaseConfig);
