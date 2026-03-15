0ffa3458c61239463c951504b1f1623d9dbefd7f
triedourluck <notfortheradios@gmail.com>
2026-03-15 15:20:50 -0300

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,1 +1,17 @@
+import { db } from './firebase-config.js';
+import {
+  collection,
+  addDoc
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
+window.publishPost = async function() {
+  const text = document.getElementById('postText').value;
+  const image = document.getElementById('imageUrl').value;
+
+  await addDoc(collection(db, "posts"), {
+    text,
+    image
+  });
+
+  alert("Publicado");
+}
