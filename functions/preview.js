e243e911dd14b43b0db4d0800d7c85c0c7355cbb
triedourluck <notfortheradios@gmail.com>
2026-03-17 05:53:18 -0300

    Update preview.js

--- functions/preview.js
+++ functions/preview.js
@@ -1,10 +1,6 @@
 exports.handler = async (event) => {
   const id = event.queryStringParameters.id || "";
 
-  const title = "The Quiet Club";
-  const description = "A quiet place to read.";
-  const image = "https://thequietclub.site/default.jpg";
-
   return {
     statusCode: 200,
     headers: {
@@ -14,9 +10,9 @@ exports.handler = async (event) => {
       <!DOCTYPE html>
       <html>
       <head>
-        <meta property="og:title" content="${title}">
-        <meta property="og:description" content="${description}">
-        <meta property="og:image" content="${image}">
+        <meta property="og:title" content="The Quiet Club">
+        <meta property="og:description" content="A note from The Quiet Club">
+        <meta property="og:image" content="https://thequietclub.site/default.jpg">
         <meta property="og:type" content="article">
         <meta name="twitter:card" content="summary_large_image">
         <meta http-equiv="refresh" content="0; url=https://thequietclub.site/post.html?id=${id}">
